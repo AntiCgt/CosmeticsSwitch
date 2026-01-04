1c6b5cdf8b1655aa1dfeb2c57e4b947b8b814aa9
Danil Cheukov <danik4eksup@gmail.com>
2026-01-04 09:48:54 +0700

    Добавьте файлы проекта.

---
+++ InvisibleCosmetics/GunSystem.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using UnityEngine;
+using GorillaTag;
+using GorillaLocomotion;
+
+namespace CosmeticsSwitch.GunSystem
+{
+    public static class SimpleGun
+    {
+        // ========== ОСНОВНЫЕ ПЕРЕМЕННЫЕ ==========
+        public static bool isShooting = false;
+        public static GameObject GunPointer = null;
+        public static LineRenderer GunLine = null;
+
+        // Настройки (можно менять если нужно)
+        public static float rayLength = 50f;
+        public static float pointerSize = 0.1f;
+        public static float lineWidth = 0.01f;
+        public static Color lineColor = Color.red;
+        public static Color pointerColor = Color.red;
+
+        // ========== ОСНОВНОЙ МЕТОД ==========
+
+        /// <summary>
+        /// Рендерит луч из правой руки и возвращает информацию о попадании
+        /// </summary>
+        public static (RaycastHit hit, GameObject pointer) RenderGun()
+        {
+            // 1. Получаем позицию правой руки
+            Transform rightHand = GorillaTagger.Instance.rightHandTransform;
+            Vector3 startPosition = rightHand.position;
+            Vector3 direction = rightHand.forward;
+
+            // 2. Делаем рейкаст
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(startPosition, direction, out hit, rayLength, GetLayerMask());
+
+            // 3. Определяем конечную точку
+            Vector3 endPosition = hasHit ? hit.point : startPosition + direction * rayLength;
+
+            // 4. Создаем/обновляем визуальные элементы
+            CreatePointer(endPosition);
+            CreateLine(startPosition, endPosition);
+
+            // 5. Возвращаем результат
+            return (hit, GunPointer);
+        }
+
+        /// <summary>
+        /// Скрывает луч и указатель
+        /// </summary>
+        public static void HideGun()
+        {
+            if (GunPointer != null) GunPointer.SetActive(false);
+            if (GunLine != null) GunLine.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Полностью очищает объекты оружия
+        /// </summary>
+        public static void Cleanup()
+        {
+            if (GunPointer != null) GameObject.Destroy(GunPointer);
+            if (GunLine != null) GameObject.Destroy(GunLine.gameObject);
+
+            GunPointer = null;
+            GunLine = null;
+        }
+
+        // ========== ВСПОМОГАТЕЛЬНЫЕ МЕТОДЫ ==========
+
+        private static void CreatePointer(Vector3 position)
+        {
+            // Создаем указатель если его нет
+            if (GunPointer == null)
+            {
+                GunPointer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                GameObject.Destroy(GunPointer.GetComponent<Collider>()); // Убираем коллайдер
+
+                // Настраиваем материал
+                Renderer renderer = GunPointer.GetComponent<Renderer>();
+                renderer.material = new Material(Shader.Find("GUI/Text Shader"));
+                renderer.material.color = pointerColor;
+            }
+
+            // Обновляем позицию и размер
+            GunPointer.SetActive(true);
+            GunPointer.transform.position = position;
+            GunPointer.transform.localScale = Vector3.one * pointerSize;
+        }
+
+        private static void CreateLine(Vector3 start, Vector3 end)
+        {
+            // Создаем линию если ее нет
+            if (GunLine == null)
+            {
+                GameObject lineObj = new GameObject("GunLine");
+                GunLine = lineObj.AddComponent<LineRenderer>();
+
+                // Настраиваем линию
+                GunLine.material = new Material(Shader.Find("GUI/Text Shader"));
+                GunLine.startColor = lineColor;
+                GunLine.endColor = lineColor;
+                GunLine.startWidth = lineWidth;
+                GunLine.endWidth = lineWidth;
+                GunLine.useWorldSpace = true;
+            }
+
+            // Обновляем позиции
+            GunLine.gameObject.SetActive(true);
+            GunLine.positionCount = 2;
+            GunLine.SetPosition(0, start);
+            GunLine.SetPosition(1, end);
+        }
+
+        private static int GetLayerMask()
+        {
+            // Создаем маску слоев (исключаем невидимые слои)
+            int layerMask = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                string layerName = LayerMask.LayerToName(i);
+                if (!string.IsNullOrEmpty(layerName))
+                {
+                    // Исключаем слои которые не должны быть видны
+                    if (!layerName.Contains("Ignore") &&
+                        !layerName.Contains("Transparent") &&
+                        !layerName.Contains("UI") &&
+                        !layerName.Contains("Water"))
+                    {
+                        layerMask |= 1 << i;
+                    }
+                }
+            }
+
+            return layerMask;
+        }
+
+        /// <summary>
+        /// Проверяет, попал ли луч в игрока
+        /// </summary>
+        public static VRRig GetHitPlayer(RaycastHit hit)
+        {
+            if (hit.collider != null)
+            {
+                return hit.collider.GetComponentInParent<VRRig>();
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Проверяет, попал ли луч в локального игрока (себя)
+        /// </summary>
+        public static bool IsHitLocalPlayer(RaycastHit hit)
+        {
+            VRRig rig = GetHitPlayer(hit);
+            if (rig != null)
+            {
+                return rig == GorillaTagger.Instance.offlineVRRig;
+            }
+            return false;
+        }
+    }
+}
---
+++ InvisibleCosmetics/Main.cs
@@ -0,0 +1,176 @@
+﻿using BepInEx;
+using CosmeticsSwitch.GunSystem;
+using GorillaNetworking;
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using static GorillaNetworking.CosmeticsController;
+using static PerfTestGorillaSlot;
+
+namespace CosmeticsSwitch
+{
+    [BepInPlugin("AC.CosmeticsSwitch", "Cosmetics Switch", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private Dictionary<VRRig, bool> playerCosmeticsState = new Dictionary<VRRig, bool>();
+        private bool myCosmeticsEnabled = true;
+        void Awake()
+        {
+            Harmony harmony = new Harmony("AC.CosmeticsSwitch");
+            harmony.PatchAll();
+            Debug.Log("This code working");
+        }
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+
+            if (ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f)
+            {
+                var (hit, pointer) = SimpleGun.RenderGun();
+
+                if (hit.collider != null)
+                {
+                    var hitRig = SimpleGun.GetHitPlayer(hit);
+                    if (hitRig != null && !hitRig.isLocal)
+                    {
+                        // ПРОВЕРЯЕМ В СЛОВАРЕ
+                        if (playerCosmeticsState.TryGetValue(hitRig, out bool currentState))
+                        {
+                            // Есть в словаре - переключаем
+                            if (currentState)
+                            {
+                                // Было включено - выключаем
+                                foreach (var item in hitRig.cosmeticSet.items)
+                                {
+                                    hitRig.cosmeticsObjectRegistry?.Cosmetic(item.displayName)?.DisableItem((CosmeticSlots)item.itemCategory);
+                                }
+                                playerCosmeticsState[hitRig] = false;
+                            }
+                            else
+                            {
+                                // Было выключено - включаем
+                                foreach (var item in hitRig.cosmeticSet.items)
+                                {
+                                    hitRig.cosmeticsObjectRegistry?.Cosmetic(item.displayName)?.EnableItem(GetSlotByItemCategory(item), hitRig);
+                                }
+                                playerCosmeticsState[hitRig] = true;
+                            }
+                        }
+                        else
+                        {
+                            // Нет в словаре - первый раз, выключаем
+                            foreach (var item in hitRig.cosmeticSet.items)
+                            {
+                                hitRig.cosmeticsObjectRegistry?.Cosmetic(item.displayName)?.DisableItem((CosmeticSlots)item.itemCategory);
+                            }
+                            playerCosmeticsState[hitRig] = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                SimpleGun.HideGun();
+            }
+
+
+            if (UnityInput.Current.GetKeyDown(KeyCode.U))
+            {
+                foreach (CosmeticsController.CosmeticItem item in VRRig.LocalRig.cosmeticSet.items)
+                {
+                    var registry = GorillaTagger.Instance.offlineVRRig.cosmeticsObjectRegistry;
+                    var cosmeticInstance = registry.Cosmetic(item.displayName);
+
+                    cosmeticInstance.EnableItem(GetSlotByItemCategory(item), VRRig.LocalRig);
+                }
+            }
+            ;
+
+            if (UnityInput.Current.GetKeyDown(KeyCode.I))
+            {
+                foreach (CosmeticsController.CosmeticItem item in VRRig.LocalRig.cosmeticSet.items)
+                {
+                    var registry = GorillaTagger.Instance.offlineVRRig.cosmeticsObjectRegistry;
+                    var cosmeticInstance = registry.Cosmetic(item.displayName);
+
+                    DisableCosmeticsForPlayer(VRRig.LocalRig);
+                }
+            }
+            ;
+
+            if (UnityInput.Current.GetKeyDown(KeyCode.O))
+            {
+                myCosmeticsEnabled = !myCosmeticsEnabled;
+
+                if (myCosmeticsEnabled)
+                {
+                    foreach (CosmeticsController.CosmeticItem item in VRRig.LocalRig.cosmeticSet.items)
+                    {
+                        var registry = GorillaTagger.Instance.offlineVRRig.cosmeticsObjectRegistry;
+                        var cosmeticInstance = registry.Cosmetic(item.displayName);
+
+                        cosmeticInstance.EnableItem(GetSlotByItemCategory(item), VRRig.LocalRig);
+                    }
+                    Debug.Log("Cosmetics ON");
+                }
+                else
+                {
+                    foreach (CosmeticsController.CosmeticItem item in VRRig.LocalRig.cosmeticSet.items)
+                    {
+                        var registry = GorillaTagger.Instance.offlineVRRig.cosmeticsObjectRegistry;
+                        var cosmeticInstance = registry.Cosmetic(item.displayName);
+
+                        DisableCosmeticsForPlayer(VRRig.LocalRig);
+                    }
+                    Debug.Log("Cosmetics OFF");
+                }
+            }
+        }
+
+        void OnDestroy()
+        {
+
+        }
+
+        CosmeticSlots GetSlotByItemCategory(CosmeticItem item)
+        {
+            switch (item.itemCategory)
+            {
+                case CosmeticCategory.Hat:
+                    return CosmeticSlots.Hat;
+                case CosmeticCategory.Face:
+                    return CosmeticSlots.Face;
+                case CosmeticCategory.Badge:
+                    return CosmeticSlots.Badge;
+                case CosmeticCategory.Arms:
+                    return CosmeticSlots.Arms;
+                case CosmeticCategory.Chest:
+                    return CosmeticSlots.Chest;
+                case CosmeticCategory.Fur:
+                    return CosmeticSlots.Fur;
+                case CosmeticCategory.TagEffect:
+                    return CosmeticSlots.TagEffect;
+                case CosmeticCategory.Shirt:
+                    return CosmeticSlots.Shirt;
+                case CosmeticCategory.Pants:
+                    return CosmeticSlots.Pants;
+                case CosmeticCategory.Back:
+                    return CosmeticSlots.Back;
+                default:
+                    return CosmeticSlots.Hat; // Значение по умолчанию
+            }
+        }
+        void DisableCosmeticsForPlayer(VRRig playerRig)
+        {
+
+            foreach (var item in playerRig.cosmeticSet.items)
+            {
+                var cosmetic = playerRig.cosmeticsObjectRegistry?.Cosmetic(item.displayName);
+                cosmetic?.DisableItem((CosmeticSlots)item.itemCategory);
+            }
+        }
+    }
+}
